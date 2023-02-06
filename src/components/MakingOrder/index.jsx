ec2b3ae3f8c2a2a176e6cabb39e52a2755db6a50
RomanKVLK <romnik20021994@gmail.com>
2023-02-06 16:29:25 +0300

    MalingOrder style

--- src/components/MakingOrder/index.jsx
+++ src/components/MakingOrder/index.jsx
@@ -1,70 +1,107 @@
+import styles from './MakingOrder.module.scss';
+
 const MakingOrder = () => {
   return (
-    <section>
+    <section className={styles.sectionBox}>
       <h2>Оформление заказа</h2>
-      <h3>Персональные данные:</h3>
-      <input type='name' placeholder='Ваше имя*' />
-      <input type='lastname' placeholder='Ваша фамилия*' />
-      <input type='email' placeholder='Ваш e-mail*' />
-      <input type='tel' placeholder='Ваш телефон*' />
-      <h3>Способ доставки:</h3>
-      <div>
-        <h4>По Беларуси:</h4>
-        <form action='radio'>
-          <label>
-            <input
-              type='radio'
-              value='Самовывоз - вул. Большая Васильковская 14(м. Льва Толстого)'
-            />
-            <span>Самовывоз - вул. Большая Васильковская 14(м. Льва Толстого)</span>
-          </label>
-          <label>
-            <input type='radio' value='Белпошта' />
-            <span>Белпошта</span>
-          </label>
-        </form>
-      </div>
-      <div>
-        <h4>Международная доставка:</h4>
-        <label>
-          <input type='radio' value='Белпочта / 1-3 недели / 30$' />
-          <span>Белпочта / 1-3 недели / 30$</span>
-        </label>
-        <label>
-          <input type='radio' value='DHL / 3-7 дней / 60$' />
-          <span>DHL / 3-7 дней / 60$</span>
-        </label>
-      </div>
-      <div>
-        <h3>Адрес доставки:</h3>
-        <input type='text' placeholder='Город*' />
-        <input type='address' placeholder='Отделение почты*' />
-      </div>
-      <div>
-        <label>
-          <input type='radio' value='Полная предоплата через ЕРИП' />
-          <span>Полная предоплата через ЕРИП</span>
-        </label>
-        <label>
-          <input type='radio' value='Денежным переводом  Visa/MasterCard' />
-          <span>Денежным переводом Visa/MasterCard</span>
-        </label>
-        <label>
-          <input type='radio' value='Наложенным платежом в отделении Белпочты' />
-          <span>Наложенным платежом в отделении Белпочты</span>
-        </label>
-      </div>
-      <div>
-        <h3>Использование бонусного счёта:</h3>
-        <input type='text' placeholder='Сумма списания бонусов*' />
-      </div>
-      <div>
-        <ul>
-          <li>Войти в личный кабинет</li>
-          <li>УСЛОВИЯ ДОСТАВКИ</li>
-          <li>УСЛОВИЯ ОБМЕНА И ВОЗВРАТА</li>
-          <li>ИНФОРМАЦИЯ ОБ ОПЛАТЕ</li>
-        </ul>
+      <div className={styles.wrapper}>
+        <div className={styles.wrapperPersonal}>
+          <h3>Персональные данные:</h3>
+          <div className={styles.wrapperInputs}>
+            <input className={styles.inputPerson} type="name" placeholder="Ваше имя*" />
+            <input className={styles.inputPerson} type="lastname" placeholder="Ваша фамилия*" />
+            <input className={styles.inputPerson} type="email" placeholder="Ваш e-mail*" />
+            <input className={styles.inputPerson} type="tel" placeholder="Ваш телефон*" />
+          </div>
+          <h3>Способ доставки:</h3>
+          <div className={styles.wrapperDelivery}>
+            <div className={styles.wrapperDeliveryBox}>
+              <h4>По Беларуси:</h4>
+              <form className={styles.form}>
+                <label>
+                  <input
+                    type="radio"
+                    value="Самовывоз - вул. Большая Васильковская 14(м. Льва Толстого)"
+                  />
+                  <span>
+                    Самовывоз - вул. Большая Васильковская 14
+                    <br />
+                    (м. Льва Толстого)
+                  </span>
+                </label>
+                <label>
+                  <input type="radio" value="Белпошта" />
+                  <span>Белпошта</span>
+                </label>
+              </form>
+            </div>
+            <div className={styles.wrapperDeliveryBox}>
+              <h4>Международная доставка:</h4>
+              <form className={styles.form}>
+                <label>
+                  <input type="radio" value="Белпочта / 1-3 недели / 30$" />
+                  <span>Белпочта / 1-3 недели / 30$</span>
+                </label>
+                <label>
+                  <input type="radio" value="DHL / 3-7 дней / 60$" />
+                  <span>DHL / 3-7 дней / 60$</span>
+                </label>
+              </form>
+            </div>
+          </div>
+          <div className={styles.inputAddress}>
+            <h3>Адрес доставки:</h3>
+            <div>
+              <input className={styles.inputPerson} type="text" placeholder="Город*" />
+              <input className={styles.inputPerson} type="address" placeholder="Отделение почты*" />
+            </div>{' '}
+          </div>
+          <div>
+            <label>
+              <input type="radio" value="Полная предоплата через ЕРИП" />
+              <span>Полная предоплата через ЕРИП</span>
+            </label>
+            <label>
+              <input type="radio" value="Денежным переводом  Visa/MasterCard" />
+              <span>Денежным переводом Visa/MasterCard</span>
+            </label>
+            <label>
+              <input type="radio" value="Наложенным платежом в отделении Белпочты" />
+              <span>Наложенным платежом в отделении Белпочты</span>
+            </label>
+          </div>
+          <div>
+            <h3>Использование бонусного счёта:</h3>
+            <input type="text" placeholder="Сумма списания бонусов*" />
+          </div>
+        </div>
+        <div className={styles.wrapperOrder}>
+          <ul>
+            <li>Войти в личный кабинет</li>
+            <li>УСЛОВИЯ ДОСТАВКИ</li>
+            <li>УСЛОВИЯ ОБМЕНА И ВОЗВРАТА</li>
+            <li>ИНФОРМАЦИЯ ОБ ОПЛАТЕ</li>
+          </ul>
+          <ul>
+            <li>
+              <p>ДОСТАВКА:</p>
+              <b>По тарифам перевозчика</b>
+            </li>
+            <li>
+              <p>БОНУСЫ:</p>
+              <b>-69 р</b>
+            </li>
+            <li>
+              <p>ИТОГО:</p>
+              <b>15250 грн</b>
+            </li>
+          </ul>
+          <button>ОФОРМИТЬ ЗАКАЗ</button>
+          <p>
+            Нажимая на кнопку «оплатить заказ», я принимаю условия публичной оферты и политики
+            конфиденциальности
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -8,13 +8,16 @@ const Cart = () => {
     <main className={styles.container}>
       <div>
         <div className={styles.pagination}>
-          Главная <img src='/img/little-arrow.svg' alt='Arrow' /> Корзина
+          Главная <img src="/img/little-arrow.svg" alt="Arrow" /> Корзина
         </div>
         <h2 className={styles.title}>Ваш заказ</h2>
       </div>
       <section>
         <CartItem />
         <CartItem />
+        <p className={styles.totalPrice}>
+          К оплате:<b>15250 p</b>
+        </p>
       </section>
       <section>
         <MakingOrder />
