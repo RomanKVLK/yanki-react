1e6fb57fac6511712a250e18dcea8181605607fa
RomanKVLK <romnik20021994@gmail.com>
2023-02-14 19:23:49 +0300

    db fix 3

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -6,7 +6,7 @@ const ProductItem = ({ imageUrl, title, price }) => {
       <button className={styles.favorites}>
         <img src='/img/heart.svg' alt='Like' />
       </button>
-      <img src={imageUrl[3]} alt='Coat' width={310} />
+      <img src={imageUrl[0]} alt='Coat' width={310} />
       <h2>{title}</h2>
       <b>{price} p</b>
       {}
